132f24ddf4692382a579724da28d55935f59226e
sriv <srikanth.ddit@gmail.com>
2015-02-13 13:14:28 +0530

    moved plugin methods to common

--- common.go
+++ common.go
@@ -3,6 +3,7 @@ package common
 
 import (
 	"archive/zip"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/dmotylev/goproperties"
@@ -779,4 +780,26 @@ func fileExists(url string) bool {
 		return false
 	}
 	return true
+}
+
+func GetPluginProperties(jsonPropertiesFile string) (map[string]interface{}, error) {
+	pluginPropertiesJson, err := ioutil.ReadFile(jsonPropertiesFile)
+	if err != nil {
+		fmt.Printf("Could not read %s: %s\n", filepath.Base(jsonPropertiesFile), err)
+		return nil, err
+	}
+	var pluginJson interface{}
+	if err = json.Unmarshal([]byte(pluginPropertiesJson), &pluginJson); err != nil {
+		fmt.Printf("Could not read %s: %s\n", filepath.Base(jsonPropertiesFile), err)
+		return nil, err
+	}
+	return pluginJson.(map[string]interface{}), nil
+}
+
+func GetGaugePluginVersion(pluginName string) (string, error) {
+	pluginProperties, err := getPluginProperties(fmt.Sprintf("%s.json", pluginName))
+	if err != nil {
+		return nil, error(fmt.Sprintf("Failed to get gauge %s properties file. %s", pluginName, err))
+	}
+	return pluginProperties["version"].(string), nil
 }
