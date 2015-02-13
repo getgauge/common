b4bf957efd62abac032db975bd7f81e172e08cdd
sriv <srikanth.ddit@gmail.com>
2015-02-13 12:57:56 +0530

    change method names to make them available outside the package

--- properties/properties.go
+++ properties/properties.go
@@ -7,7 +7,7 @@ import (
 	"path/filepath"
 )
 
-func getPluginProperties(jsonPropertiesFile string) (map[string]interface{}, error) {
+func GetPluginProperties(jsonPropertiesFile string) (map[string]interface{}, error) {
 	pluginPropertiesJson, err := ioutil.ReadFile(jsonPropertiesFile)
 	if err != nil {
 		fmt.Printf("Could not read %s: %s\n", filepath.Base(jsonPropertiesFile), err)
@@ -21,7 +21,7 @@ func getPluginProperties(jsonPropertiesFile string) (map[string]interface{}, err
 	return pluginJson.(map[string]interface{}), nil
 }
 
-func getGaugePluginVersion(pluginName string) (string, error) {
+func GetGaugePluginVersion(pluginName string) (string, error) {
 	pluginProperties, err := getPluginProperties(fmt.Sprintf("%s.json", pluginName))
 	if err != nil {
 		return nil, error(fmt.Sprintf("Failed to get gauge %s properties file. %s", pluginName, err))
