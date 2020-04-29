b5f3fadf2bd339cbf45947296c45cabab70ce3eb
Srikanth <srikanth.ddit@gmail.com>
2020-04-29 14:01:45 +0530

    Changed license to Apache2.0 (#6)
    * Changed license to Apache2.0
    
    Signed-off-by: sriv <srikanth.ddit@gmail.com>

--- common_test.go
+++ common_test.go
@@ -1,19 +1,8 @@
-// Copyright 2015 ThoughtWorks, Inc.
-
-// This file is part of getgauge/common.
-
-// getgauge/common is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// getgauge/common is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with getgauge/common.  If not, see <http://www.gnu.org/licenses/>.
+/*----------------------------------------------------------------
+ *  Copyright (c) ThoughtWorks, Inc.
+ *  Licensed under the Apache License, Version 2.0
+ *  See LICENSE in the project root for license information.
+ *----------------------------------------------------------------*/
 
 package common
 
@@ -26,7 +15,7 @@ import (
 	"strings"
 	"testing"
 
-	. "github.com/go-check/check"
+	. "gopkg.in/check.v1"
 )
 
 const dummyProject = "dummy_proj"
